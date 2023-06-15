163aa7401bb95b731a13ee2e940e2a3828cdeb77
devops <devops@smartsoftint.com>
2023-06-14 22:10:31 -0500

    create explorer component

---
+++ src/app/modules/profile/explorer/explorer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explorer',
+  templateUrl: './explorer.component.html',
+  styleUrls: ['./explorer.component.sass']
+})
+export class ExplorerComponent {
+
+}
