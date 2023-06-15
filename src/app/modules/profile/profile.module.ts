47ba04bbb9618002865da20ad9a3c6d7b7ede906
devops <devops@smartsoftint.com>
2023-06-14 22:10:18 -0500

    create profile module and routing

---
+++ src/app/modules/profile/profile.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExplorerComponent } from './explorer/explorer.component';
+
+
+
+@NgModule({
+  declarations: [
+    ExplorerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProfileModule { }
