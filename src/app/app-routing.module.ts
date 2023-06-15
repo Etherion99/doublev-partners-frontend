1e492f13225e1fe236232c4a8521f2eef225be1b
devops <devops@smartsoftint.com>
2023-06-14 22:12:32 -0500

    configure general routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/components/errors/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: '',
+    redirectTo: '/profile/explorer',
+    pathMatch: 'full'
+  },
+  { 
+    path: '404',
+    component: NotFoundComponent
+  },
+  { 
+    path: '**',
+    redirectTo: '404'
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
