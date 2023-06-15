4192333342ea57cf92f188faecf500b186fd53bc
devops <devops@smartsoftint.com>
2023-06-15 06:06:53 -0500

    create basic explorer logic

--- src/app/modules/profile/explorer/explorer.component.ts
+++ src/app/modules/profile/explorer/explorer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { forbidenWordValidator } from 'src/app/shared/validators/forbidenWordValidator';
 
 @Component({
   selector: 'app-explorer',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./explorer.component.sass']
 })
 export class ExplorerComponent {
-
+  searchTextControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(4),
+    forbidenWordValidator('doublevpartners')
+  ]);
 }
